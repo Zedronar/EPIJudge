7d75875e79b56ccddd793522ea40102a342bb0e8
Axel Prieto <prietoaxel@gmail.com>
2020-11-01 10:37:17 +0000

    minor changes

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -1,9 +1,29 @@
 from test_framework import generic_test
 
 
+def parity_brute_force(x: int) -> int:
+    result = 0
+    while x:
+        result ^= x & 1
+        x >>= 1
+    return result
+
+# Erasing the lowest set bit in a word in a single operation
+# x & (x-1)
+
+# 2 & 1 =
+# 10 & 01 = 00
+
+# 3 & 2 =
+# 11 & 10 = 10
+
+
 def parity(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    result = 0
+    while x:
+        result ^= 1
+        x &= x - 1  # Drops the lowest set bit of x.
+    return result
 
 
 if __name__ == '__main__':
